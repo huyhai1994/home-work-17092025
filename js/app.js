e1def7aa50d64cf06ad3b37c82bedf77549b114c
huyhai1994 <nguyenhuyhai926@gmail.com>
2025-09-26 21:17:12 +0700

    add new item get from form submit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
 const buttonAdd = document.getElementById("button-add");
-const modal =document.getElementById("modal");
+const modal = document.getElementById("modal");
 const buttonClose = document.body.querySelector(".close");
+const tableBody = document.body.querySelector(".table-body");
+const submitButton = document.body.querySelector('.submit-button');
+const  formModal = document.body.querySelector('.modal-form');
 
+const data = [
+  {name: 'Banana', price: "0.99"},
+  {name: 'Apple', price: "1.99"},
+  {name: 'Orange', price: "1.49"},
+  {name: 'Grapes', price: "2.99"},
+  {name: 'Mango', price: "1.79"},
+  {name: 'Pineapple', price: "3.49"},
+  {name: 'Strawberry', price: "2.59"},
+  {name: 'Blueberry', price: "2.89"},
+  {name: 'Watermelon', price: "4.99"},
+  {name: 'Peach', price: "1.59"},
+  {name: 'Kiwi', price: "1.29"},
+  {name: 'Pear', price: "1.39"},
+  {name: 'Plum', price: "1.19"},
+]
 buttonAdd.addEventListener('click',
   () => {
     modal.style.display = "block";
@@ -12,4 +30,34 @@ buttonClose.addEventListener('click',
     modal.style.display = "none";
   }
 )
+
+formModal.addEventListener('submit',
+  (e) => {
+    e.preventDefault();
+    let formData = new FormData(e.target)
+    const formProps = Object.fromEntries(formData);
+    const newData =  {name: formProps?.name, price: formProps?.price}
+    data.push(newData);
+    renderElement(data);
+  }
+)
+
+renderElement(data);
+
+
+function renderElement(data){
+  data.forEach(
+    d => {
+      const tableRow = document.createElement('tr');
+      const tableData1 = document.createElement('td');
+      const tableData2 = document.createElement('td');
+      tableData1.innerText = d.name;
+      tableData2.innerText = d.price;
+      tableRow.append(tableData1);
+      tableRow.append(tableData2);
+      tableBody.append(tableRow);
+    }
+  )
+}
+
 
